57ded8dba5592d2d266d5919206797caabe67d6d
Ismailmln <126384455+Ismailmln@users.noreply.github.com>
2023-04-19 14:46:53 +0600

    DZ4_Ismail_25-01

---
+++ DZ4_Ismail_25-01/main.js
@@ -0,0 +1,59 @@
+// const posX = document.querySelector('#posX')
+// const posY = document.querySelector('#posY')
+// document.querySelector('#mouse').onmousemove = (MouseEvent) => {
+//     posX.innerHTML = event.x
+//     posY.innerHTML = event.y
+// }
+
+// document.querySelector('#mouse').onmousemove = function (event) {
+//     event = event || window.event
+//     document.querySelector('#offx').innerHTML = event.offsetX;
+//     document.querySelector('#offy').innerHTML = event.offsetY;
+// }
+
+// let svetofor = {
+//     red:'Красный',
+//     green:'Зеленый'
+// }
+// let svetofor = prompt('Введите цвет')
+// if (color === svetofor.red) {
+//     alert('STOP')
+// }
+// else if (color === svetofor.green) {
+//     alert('GO')
+// }
+
+// const counter = document.getElementById('counter')
+// const increment = document.getElementById('increment')
+// const reset = document.getElementById('reset')
+// const decrement = document.getElementById('decrement')
+//
+// let counterValue = 0
+//
+// function click () {
+//     counter.innerHTML = counterValue
+//     if (counterValue > 0) {
+//         counter.style.color = 'green'
+//     }else if (counterValue === 0) {
+//         counter.style.color = 'black'
+//     }else {
+//         counter.style.color = 'red'
+//     }
+// }
+// click()
+// increment.addEventListener("click", function () {
+//     counterValue++
+//     click()
+// })
+//
+// reset.addEventListener('click', function () {
+//     counterValue = 0
+//     click()
+// })
+//
+// decrement.addEventListener('click', function () {
+//     counterValue--
+//     counter.style.color = 'red'
+//     click()
+// })
+
